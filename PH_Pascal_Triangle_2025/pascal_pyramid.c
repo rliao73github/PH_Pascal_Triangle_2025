43e3d7a3954f53b4c8062efec82a812cd4bd81ed
Ron Liao <rliao73@gmail.com>
2025-10-11 20:43:15 -0400

    second commit

--- PH_Pascal_Triangle_2025/pascal_pyramid.c
+++ PH_Pascal_Triangle_2025/pascal_pyramid.c
@@ -1,29 +1,33 @@
 /* pascal_pyramid.c
    Build Pascal's Triangle in a 2D array and print in a centered pyramid.
    Compile: gcc -std=c11 -O2 pascal_pyramid.c -o pascal_pyramid
-   Run:     ./pascal_pyramid 5
+   Run:     ./pascal_pyramid 5     -> 5 rows
+            ./pascal_pyramid       -> defaults to 10 rows
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX 50  // max rows
+#define DEFAULT_ROWS 10
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <rows>\n", argv[0]);
-        return 1;
-    }
+    int rows = DEFAULT_ROWS;  // default value
 
-    int rows = atoi(argv[1]);
-    if (rows <= 0 || rows > MAX) {
-        fprintf(stderr, "Please enter a positive integer (1-%d).\n", MAX);
+    if (argc == 2) {
+        rows = atoi(argv[1]);
+        if (rows <= 0 || rows > MAX) {
+            fprintf(stderr, "Please enter a positive integer (1–%d).\n", MAX);
+            return 1;
+        }
+    } else if (argc > 2) {
+        fprintf(stderr, "Usage: %s [rows]\n", argv[0]);
         return 1;
     }
 
     long long pascal[MAX][MAX] = {0};
 
-    // Build Pascal’s Triangle using array
+    // Build Pascal’s Triangle
     for (int i = 0; i < rows; i++) {
         pascal[i][0] = pascal[i][i] = 1;
         for (int j = 1; j < i; j++) {
@@ -33,14 +37,14 @@ int main(int argc, char *argv[]) {
 
     // Print as pyramid
     for (int i = 0; i < rows; i++) {
-        // Center alignment: print spaces
+        // Center alignment: spaces
         for (int s = 0; s < rows - i - 1; s++) {
-            printf("   "); // three spaces per missing number
+            printf("   ");
         }
 
         // Print row values
         for (int j = 0; j <= i; j++) {
-            printf("%3lld   ", pascal[i][j]); // width 3 for alignment
+            printf("%3lld   ", pascal[i][j]);
         }
         printf("\n");
     }
