b461fa77e1b2ceeaf62b522704763bad8c1ea894
Ron Liao <rliao73@gmail.com>
2025-10-11 20:04:18 -0400

    Initial Commit

---
+++ PH_Pascal_Triangle_2025/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PH_Pascal_Triangle_2025
+//
+//  Created by Rong Liao on 2025-10-11.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
